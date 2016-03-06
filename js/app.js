5cd4497f7af384841d308d80c8b8b3e98e68c6ff
sarusia00 <sarus00@gmail.com>
2016-03-06 12:40:40 +0100

    Zadanie dodatkowe - lista zakupow

--- js/app.js
+++ js/app.js
@@ -1,105 +1,85 @@
 document.addEventListener("DOMContentLoaded", function() {
-//moje zdefiniowane zmenne
-  var addItem = document.querySelector(".btn");
-  var newTab = document.querySelector(".adding")
-  console.log(newTab)
-  
-   var makeText = document.createTextNode("example");
-  var newTr = document.createElement("tr");
-    console.log(newTr);
-  var table = document.querySelector("table"); 
-    console.log(table)
-    var toPay = document.querySelector(".toPay")
-    var removeBtn = document.createElement("button");
-    removeBtn.innerHTML="Skreśl przedmiot";
     
-    var quantity=document.querySelector(".quantity");
-    console.log(quantity)
-    var price = document.querySelector(".price");
-    console.log(price)
-    var product = document.querySelector(".product")
-    console.log(product)
-    //var suma = document.querySelector()
-    //tutaj musisz pomnożyć ilość razy cenę produktu
-    var howMuch = document.querySelector(".howMuch")
-   console.log(howMuch)
+//moje zdefiniowane zmenne
+  var addItem = document.querySelector("#addBtn");
+  var newTab = document.querySelector(".adding");
+  var table = document.querySelector("table"); 
+  var strikeTr = document.getElementsByClassName("strike")
+  var quantityInput =document.querySelector("#quantity"); 
+  var priceInput = document.querySelector("#price");
+  var productInput = document.querySelector("#product")
+  var howMuch = document.querySelector("#howMuch")
+
 
  function addProduct() {
-    var howMuch = document.querySelector(".howMuch") 
     var removeBtn = document.createElement("button");
     removeBtn.innerHTML="Skreśl przedmiot";
+   
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
+     
+    cell5.classList.add("altogether");
+     //zawartość mojej tabeli
     cell1.appendChild(removeBtn); 
-    cell2.innerHTML = product.value
-    cell3.innerHTML = parseFloat(quantity.value);
-    cell4.innerHTML = parseFloat(price.value);
-    var onePro = parseFloat(quantity.value)*parseFloat(price.value); 
-    cell5.innerHTML = onePro;
-     
-      //jak dodać do siebie wartość wszystkich cell5 aby otrzymać sumę
-    for (var i = 0; i<cell5.length; i++) {
-        document.querySelector("td").lastChild = cell5[i];
-    }
- }
-     
-     
-        
-    /*function findTotal(){
-    var arr = document.querySelector("td").lastChild;
-    var tot=0;
-    for(var i=0;i<arr.length;i++){
-        if (arr[i].value){
-            tot += arr[i].value;
-            howMuch.innerHTML = tot*/   
-      
- 
+    cell2.innerHTML = productInput.value
     
-/*function findTotal(){
-    var arr = document.querySelector("td").lastChild;
-    var tot=0;
-    for(var i=0;i<arr.length;i++){
-        if (arr[i].value){
-            tot += arr[i].value;
+    if (quantityInput.value !== "") {
+        cell3.innerHTML = parseFloat(quantityInput.value);
+    }
+    else {    
+        alert("Proszę podać ilość produktów");
+        cell3.innerHTML = parseFloat("0");
+    }
+     if (priceInput.value !== "") {
+        cell4.innerHTML = parseFloat(priceInput.value);
+    }
+    else {    
+        alert("Proszę podać cenę produktu");
+        cell4.innerHTML = parseFloat("0");
+    }  
+     
+     if (quantityInput.value === "" && priceInput.value === "") {
+         cell5.innerHTML = parseFloat("0");
+     }
+     else if (quantityInput.value === "" || priceInput.value === "") {
+         cell5.innerHTML = parseFloat("0");
+     }
+    else {
+        cell5.innerHTML = parseFloat(quantity.value)*parseFloat(price.value);
+    }
+     
+     //moj event dla strike:
+       removeBtn.addEventListener("click", function(event){
+           var rowToDelete = this.parentElement.parentElement;//wiersz jest dziadkiem dla mojego guzika
+           var myRow = document.getElementsByTagName("tr");
+           rowToDelete.classList.toggle("strike");
+           howMuch.innerHTML ="0";
+           
+     for (var z=0;z<myRow.length;z++) {
+        var subRowValue = myRow[z].lastChild.innerHTML;
+        if (subRowValue && !myRow[z].classList.contains("strike")) { 
+          	howMuch.innerHTML = parseFloat(howMuch.innerHTML)+parseFloat(subRowValue); 
         }
-    }*/
-    //get total
-    
-    
-   addItem.addEventListener("click", addProduct); 
-    
-    function removeProduct () {
-     var removeBtn = document.createElement("button");
-     removeBtn.innerHTML="Skreśl przedmiot";
-     var myRow = document.querySelector("tr");
-        myRow.strike();
-    }
-    
-    
-                            
-    removeBtn.addEventListener("click", removeProduct)
-    
+     }
+                  
+    });
+       
+     var toPay = document.getElementsByClassName("altogether");
+    var suma = 0;
+    for (var n=0; n<toPay.length;n++) {
+         var suma = suma + parseFloat(toPay[n].innerHTML);
+            howMuch.innerHTML = parseFloat(suma);
+        }
+        //jeśli row ma klasę strike to należy odjąć jego cell5 od całej sumy
      
-    
-      //var przekreslonyPrice = document.createElement('strike');
-      //przekreslonyPrice.innerHTML = cell2;
-      //cell4.innerHTML = przekreslonyPrice;
-   
-  
-   // function();
- /*var makeP = document.createElement("p")    
-  var para = document.querySelector("p");
-  var proba = document.querySelector(".proba")
-  var t = document.createTextNode("Test"); 
+    };
 
-  proba.appendChild(makeP);
-   makeP.appendChild(t);
-   ==============================================
-   Tu masz przykład jak działa appendChild*/
+     addItem.addEventListener("click", addProduct); 
+    
     
  
     
